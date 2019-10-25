9bbf286f75abe8ede6e3f9896ae45411697abd0d
EzeVedder <pane.ezequiel@gmail.com>
2019-10-24 22:13:25 -0300

    add button start-stop

--- ejercicio-timers/scripts/app.js
+++ ejercicio-timers/scripts/app.js
@@ -5,7 +5,25 @@ window.addEventListener('load', function () {
 
     // timers
 
-    // var tiempo = setInterval(function () {
+    var tiempo = intervalo();
+
+    var stop = document.querySelector('#stop');
+
+    stop.addEventListener('click',function(){
+        console.log('Intervalo detenido.')
+        clearInterval(tiempo);
+    });
+
+    var start = document.querySelector('#start');
+
+    start.addEventListener('click',function(){
+        console.log('Has iniciado el intervalo')
+        tiempo = intervalo();    
+    });
+
+
+
+    // var tiempo = setTimeout(function () {
 
     //     console.log('Set Interval ejecutado');
 
@@ -18,24 +36,6 @@ window.addEventListener('load', function () {
             
     //     }
     // }, 3000);
-
-
-    var tiempo = setTimeout(function () {
-
-        console.log('Set Interval ejecutado');
-
-        var titulo = document.getElementById('titulo');
-
-        if (titulo.style.fontSize == "100px") {
-            titulo.style.fontSize = "20px";
-        } else {
-            titulo.style.fontSize = "100px";
-            
-        }
-    }, 3000);
-
-
-
 
 
 
@@ -51,3 +51,20 @@ window.addEventListener('load', function () {
 //     console.log('Funcion autoejecutable');
 
 // }())
+
+function intervalo(){
+    var tiempo = setInterval(function () {
+
+    console.log('Set Interval ejecutado');
+
+    var titulo = document.getElementById('titulo');
+
+    if (titulo.style.fontSize == "100px") {
+        titulo.style.fontSize = "20px";
+    } else {
+        titulo.style.fontSize = "100px";
+        
+    }
+}, 3000);
+    return tiempo;
+}   
